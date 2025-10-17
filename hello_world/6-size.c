96304cda78a12c646c85bb4fbc6951e15499b10e
psmacabulos <patrick.macabulos@gmail.com>
2025-10-17 14:09:28 +0000

    Corrected task 4 and 6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -3,14 +3,14 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 0
  */
 int main(void)
 {
-printf("Size of a char: %lu byte(s)\n", sizeof(char));
-printf("Size of an int: %lu byte(s)\n", sizeof(int));
-printf("Size of a long int: %ln byte(s)\n", sizeof(long int));
-printf("Size of a long long int: %ln byte(s)\n", sizeof(long long int));
-printf("Size of a float: %ln byte(s)\n", sizeof(float));
+printf("Size of a char: %d byte(s)\n", (int)sizeof(char));
+printf("Size of an int: %d byte(s)\n", (int)sizeof(int));
+printf("Size of a long int: %d byte(s)\n", (int)sizeof(long int));
+printf("Size of a long long int: %d byte(s)\n", (int)sizeof(long long int));
+printf("Size of a float: %d byte(s)\n", (int)sizeof(float));
 return (0);
 }
