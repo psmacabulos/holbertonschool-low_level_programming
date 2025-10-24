d044110dca50189a190b9ee1024ca3ecd535dec0
psmacabulos <patrick.macabulos@gmail.com>
2025-10-24 23:13:42 +1100

    Created program for task 6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int numbers = 48;
+
+while (numbers <= 57)
+{
+putchar(numbers);
+numbers++;
+}
+putchar('\n');
+return (0);
+}
