3d092664133e569cffaae21882a19ac1a3bddd6a
psmacabulos <patrick.macabulos@gmail.com>
2025-10-26 12:55:11 +1100

    Created a program for task 5

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,11 +1,33 @@
 #include "main.h"
 
 /**
- * main - Entry point
+ * print_two_digits - print a number from 0 to 14 using _putchar
+ * @n: number to print
  *
- * Return: Always 0 (Success)
+ * Note: uses _putchar exactly twice in this function body
  */
-int main(void)
+void print_two_digits(int n)
 {
-	return (0);
+	if (n >= 10)
+		_putchar('1');
+	_putchar((n % 10) + '0');
+}
+
+/**
+ * more_numbers - prints 10 times the numbers from 0 to 14
+ *
+ * Allowed: exactly three _putchar occurrences in source:
+ *  - two in print_two_digits (tens and units)
+ *  - one here for the newline at the end of each line
+ */
+void more_numbers(void)
+{
+	int line, n;
+
+	for (line = 0; line < 10; line++)
+	{
+		for (n = 0; n <= 14; n++)
+			print_two_digits(n);
+		_putchar('\n');
+	}
 }
