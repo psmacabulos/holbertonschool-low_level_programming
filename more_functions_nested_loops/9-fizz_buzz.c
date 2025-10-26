0e196e3b1fe6b3c0bf6578b4a87826e0add501f6
psmacabulos <patrick.macabulos@gmail.com>
2025-10-26 20:05:35 +1100

    Created the FizzBuzz program for task 8

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
  * main - Entry point
@@ -7,5 +7,25 @@
  */
 int main(void)
 {
+	int index;
+
+	for (index = 1; index <= 100; index++)
+	{
+		if (index % 3 == 0 && index % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (index % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (index % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		printf("%d ", index);
+	}
+	printf("\n");
 	return (0);
 }
