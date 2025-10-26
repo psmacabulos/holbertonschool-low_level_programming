ff80ed90dc4871db0cea1e87bdc344fd263e32d3
psmacabulos <patrick.macabulos@gmail.com>
2025-10-26 19:37:27 +1100

    Corrected program for task 8 to address condition when size is equal to 0

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -12,6 +12,11 @@ void print_square(int size)
 	int line;
 	int row;
 
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
 	for (line = 0; line < size; line++)
 	{
 		for (row = 0; row < size; row++)
