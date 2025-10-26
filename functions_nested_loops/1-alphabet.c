b1a346736be032894a2b499d38bb4290fd712450
psmacabulos <patrick.macabulos@gmail.com>
2025-10-26 22:19:28 +1100

    Created program for task 1

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,11 +1,16 @@
 #include "main.h"
 /**
- * main - check the code
+ * print_alphabet - check the code
  *
  * Return: Always 0.
  */
-int main(void)
+void print_alphabet(void)
 {
-    print_alphabet();
-    return (0);
+	int character;
+
+	for (character = 97; character <= 122; character++)
+	{
+		_putchar(character);
+	}
+	_putchar('\n');
 }
