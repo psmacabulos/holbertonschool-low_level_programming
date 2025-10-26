059b8bebb426d64d1fb64553840d9988d4a7acdf
psmacabulos <patrick.macabulos@gmail.com>
2025-10-26 23:13:04 +1100

    Created program for task 5

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,11 +1,24 @@
 #include "main.h"
 /**
- * main - check the code
+ * print_sign - checks if n is more than 0 or not
+ *
+ * @n :number to be check what is the sign
  *
  * Return: Always 0.
  */
-int main(void)
+
+int print_sign(int n)
 {
-    print_alphabet();
-    return (0);
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	_putchar('0');
+	return (0);
 }
