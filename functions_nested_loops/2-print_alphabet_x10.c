3375804c2c866e35bb00796c859451e4906b8845
psmacabulos <patrick.macabulos@gmail.com>
2025-10-26 22:32:20 +1100

    Created the program for task 2

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,20 @@
 #include "main.h"
 /**
- * main - check the code
+ * print_alphabet_x10 - prints the alphabet 10 times
  *
  * Return: Always 0.
  */
-int main(void)
+void print_alphabet_x10(void)
 {
-    print_alphabet();
-    return (0);
+	int character;
+	int index;
+
+	for (index = 1; index <= 10; index++)
+	{
+	for (character = 97; character <= 122; character++)
+	{
+		_putchar(character);
+	}
+	_putchar('\n');
+	}
 }
