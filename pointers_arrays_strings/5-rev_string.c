bafa47a8bd0c73c4de850a3df6b1dce1566caf89
psmacabulos <patrick.macabulos@gmail.com>
2025-10-30 20:47:26 +0800

    Solved problem 5

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,11 +1,27 @@
 #include "main.h"
 
 /**
- * _5_rev_string - placeholder function derived from filename 5-rev_string.c
+ * rev_string - reverses a string in place
+ * @s: the string to reverse
  *
- * Return: 0
+ * Description: swaps characters from ends towards the center.
  */
-int _5_rev_string(void)
+void rev_string(char *s)
 {
-    return (0);
+	int i, j;
+	char tmp;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+
+	/* i now holds length; set j to last valid index */
+	j = i - 1;
+
+	for (i = 0; i < j; i++, j--)
+	{
+		tmp = s[i];
+		s[i] = s[j];
+		s[j] = tmp;
+	}
 }
