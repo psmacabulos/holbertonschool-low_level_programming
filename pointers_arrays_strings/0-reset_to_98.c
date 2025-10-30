fa68e29773c483bd033e3a412674344ae73ee7a8
psmacabulos <patrick.macabulos@gmail.com>
2025-10-30 17:03:14 +0800

    Created program for task 0

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,12 @@
 #include "main.h"
 
 /**
- * _0_reset_to_98 - placeholder function derived from filename 0-reset_to_98.c
+ * reset_to_98 - placeholder function derived from filename 0-reset_to_98.c
  *
+ * @n : pointer with integer type
  * Return: 0
  */
-int _0_reset_to_98(void)
+void reset_to_98(int *n)
 {
-    return (0);
+	*n = 98;
 }
