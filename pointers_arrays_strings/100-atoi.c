dbd70bb1f0c16ac807a25f6f0120f3a198d26953
psmacabulos <patrick.macabulos@gmail.com>
2025-10-31 13:23:53 +0800

    Corrected code and let the -fsanitize flag during compilation take care of overflow

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,16 +1,21 @@
 #include "main.h"
-#include <limits.h>
 
+/**
+ * _atoi - converts a string to an integer
+ * @s: input string
+ *
+ * Return: the integer value, or 0 if no digits found
+ */
 int _atoi(char *s)
 {
 	int i = 0;
 	int sign = 1;
-	int conv = 0;
-	int digits = 0;
+	int result = 0;
+	int started = 0;
 
 	while (s[i] != '\0')
 	{
-		if (!digits && (s[i] == '+' || s[i] == '-'))
+		if (!started && (s[i] == '-' || s[i] == '+'))
 		{
 			if (s[i] == '-')
 				sign *= -1;
@@ -19,17 +24,13 @@ int _atoi(char *s)
 		{
 			int digit = s[i] - '0';
 
-			digits = 1;
-			if (conv > (INT_MAX - digit) / 10)
-			{
-				return ((sign == 1) ? INT_MAX : INT_MIN);
-			}
+			started = 1;
 
-			conv = conv * 10 + digit;
+			result = result * 10 + digit;
 		}
-		else if (digits)
+		else if (started)
 			break;
 		i++;
 	}
-	return (conv * sign);
+	return (result * sign);
 }
