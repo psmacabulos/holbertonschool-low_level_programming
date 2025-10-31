5162fb2144041e114ab7cd36e1bb3814ed8b2fdc
psmacabulos <patrick.macabulos@gmail.com>
2025-10-31 13:02:54 +0800

    Created program for task 10

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,11 +1,35 @@
 #include "main.h"
+#include <limits.h>
 
-/**
- * _100_atoi - placeholder function derived from filename 100-atoi.c
- *
- * Return: 0
- */
-int _100_atoi(void)
+int _atoi(char *s)
 {
-    return (0);
+	int i = 0;
+	int sign = 1;
+	int conv = 0;
+	int digits = 0;
+
+	while (s[i] != '\0')
+	{
+		if (!digits && (s[i] == '+' || s[i] == '-'))
+		{
+			if (s[i] == '-')
+				sign *= -1;
+		}
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			int digit = s[i] - '0';
+
+			digits = 1;
+			if (conv > (INT_MAX - digit) / 10)
+			{
+				return ((sign == 1) ? INT_MAX : INT_MIN);
+			}
+
+			conv = conv * 10 + digit;
+		}
+		else if (digits)
+			break;
+		i++;
+	}
+	return (conv * sign);
 }
