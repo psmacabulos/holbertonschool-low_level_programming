d681a81a48960deddba006506f17fabe0ed1c68f
psmacabulos <patrick.macabulos@gmail.com>
2025-11-01 15:04:30 +0800

    Created program for task 1 combining strings up to n bytes

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,11 +1,31 @@
 #include "main.h"
 
 /**
- * _1_strncat - 
+ * _strncat - concatenates two strings using at most n bytes from src
+ * @dest: destination string to append to
+ * @src: source string to append from
+ * @n: maximum number of bytes to use from src
  *
- * Return: 0
+ * Return: pointer to resulting string dest
  */
-int _1_strncat(void)
+char *_strncat(char *dest, char *src, int n)
 {
-    return (0);
+	int dest_len = 0;
+	int i = 0;
+
+	/* Find the end of dest */
+	while (dest[dest_len] != '\0')
+		dest_len++;
+
+	/* Append up to n bytes from src */
+	while (i < n && src[i] != '\0')
+	{
+		dest[dest_len + i] = src[i];
+		i++;
+	}
+
+	/* Null-terminate the result */
+	dest[dest_len + i] = '\0';
+
+	return (dest);
 }
