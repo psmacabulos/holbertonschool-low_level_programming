2246995825714d71a2a85d59df75bca05b3d8bc2
psmacabulos <patrick.macabulos@gmail.com>
2025-11-01 19:49:58 +0800

    Created program to reverse array

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -1,11 +1,21 @@
 #include "main.h"
 
 /**
- * _4_rev_array - 
- *
+ * reverse_array - reverse the content of an array of integers
+ * @a: array
+ * @n: number of elements in the array
  * Return: 0
  */
-int _4_rev_array(void)
+
+void reverse_array(int *a, int n)
 {
-    return (0);
+
+	int tmp, i;
+
+	for (i = 0; i < n / 2; i++)
+	{
+		tmp = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = tmp;
+	}
 }
