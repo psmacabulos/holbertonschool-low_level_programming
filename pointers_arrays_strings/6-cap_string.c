de29faa38fcf504c70278c477272b813a0ac6924
psmacabulos <patrick.macabulos@gmail.com>
2025-11-01 23:41:38 +0800

    Created a program to Capitalize string

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,11 +1,36 @@
 #include "main.h"
 
 /**
- * _6_cap_string - 
+ * cap_string - capitalizes all words of a string
+ * @str: input string
  *
- * Return: 0
+ * Return: pointer to the modified string
  */
-int _6_cap_string(void)
+char *cap_string(char *str)
 {
-    return (0);
+	int i = 0;
+	int new_word = 1;
+	char sep[] = " \t\n,;.!?\"(){}";
+	int j;
+
+	while (str[i])
+	{
+		if (new_word && str[i] >= 'a' && str[i] <= 'z')
+			str[i] -= 32;
+
+		new_word = 0;
+
+		for (j = 0; sep[j]; j++)
+		{
+			if (str[i] == sep[j])
+			{
+				new_word = 1;
+				break;
+			}
+		}
+
+		i++;
+	}
+
+	return (str);
 }
