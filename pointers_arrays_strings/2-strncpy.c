2cdccac1a4017b9cddd0a9affdac5bc7db843740
psmacabulos <patrick.macabulos@gmail.com>
2025-11-01 15:47:15 +0800

    Created the program for strncpy

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,11 +1,22 @@
 #include "main.h"
 
 /**
- * _2_strncpy - 
+ * _strncpy - copies a string up to n bytes
+ * @dest: destination buffer
+ * @src: source string
+ * @n: maximum number of bytes to copy
  *
- * Return: 0
+ * Return: pointer to destination buffer
  */
-int _2_strncpy(void)
+char *_strncpy(char *dest, char *src, int n)
 {
-    return (0);
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+
+	for (; i < n; i++)
+		dest[i] = '\0';
+
+	return (dest);
 }
