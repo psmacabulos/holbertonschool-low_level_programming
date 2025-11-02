6305600446231834085ecb67fda563b39b9a6a52
psmacabulos <patrick.macabulos@gmail.com>
2025-11-02 21:37:15 +0800

    Created program to match strspn

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -1,11 +1,34 @@
 #include "main.h"
 
 /**
- * _3_strspn - 
- * @
- * Return: 0
+ * _strspn - gets the length of a prefix substring
+ * @s: input string
+ * @accept: accepted characters
+ *
+ * Return: number of bytes in the initial segment of s
+ *         which consist only of bytes from accept
  */
-int _3_strspn(void)
+unsigned int _strspn(char *s, char *accept)
 {
-    return (0);
+	unsigned int i, j;
+	int match;
+
+	for (i = 0; s[i]; i++)
+	{
+		match = 0;
+
+		for (j = 0; accept[j]; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				match = 1;
+				break;
+			}
+		}
+
+		if (!match)
+			break;
+	}
+
+	return (i);
 }
