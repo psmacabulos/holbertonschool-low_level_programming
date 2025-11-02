ddacd7382d9266227c5784c9d00de8258549a82c
psmacabulos <patrick.macabulos@gmail.com>
2025-11-02 19:38:36 +0800

    Created a program to mimic memcpy

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -1,11 +1,21 @@
 #include "main.h"
 
 /**
- * _1_memcpy - 
- * @
+ * _memcpy -copies memory area
+ * @dest: destination of the memory area
+ * @src: source of data
+ * @n : n bytes to be copied
  * Return: 0
  */
-int _1_memcpy(void)
+
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
-    return (0);
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (0);
 }
