9874010e259dbee4423426be014a18877965d711
psmacabulos <patrick.macabulos@gmail.com>
2025-11-05 22:22:04 +0800

    Solved problem 6

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,11 +1,28 @@
 #include "main.h"
 
 /**
- * _6_is_prime_number - 
- * @
- * Return: 0
+ * check_prime - helper to test divisibility recursively
+ * @n: number to test
+ * @i: current divisor
+ * Return: 1 if prime, 0 if not
  */
-int _6_is_prime_number(void)
+int check_prime(int n, int i)
 {
-    return (0);
+	if (i * i > n)
+		return (1);
+	if (n % i == 0)
+		return (0);
+	return (check_prime(n, i + 1));
+}
+
+/**
+ * is_prime_number - returns 1 if n is prime, else 0
+ * @n: number to test
+ * Return: 1 if prime, 0 if not
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (check_prime(n, 2));
 }
