684b9750e6dff741e5f9c4708fdd02263e4ee706
psmacabulos <patrick.macabulos@gmail.com>
2025-11-05 15:09:32 +0800

    Solved problem 5

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,11 +1,35 @@
 #include "main.h"
+/**
+ * _guess_root - guess the square or root by incrementing the number one by one
+ * @n: number to be computed
+ * @i: number to be compared to be the root of n
+ * Return: square root of n
+ */
+
+
+
+int _guess_root(int n, int i)
+{
+	if (i * i == n)
+		return (i);
+	if (i * i > n)
+		return (-1);
+	return (_guess_root(n, i + 1));
+}
 
 /**
- * _5_sqrt_recursion - 
- * @
- * Return: 0
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: number to be computed
+ * Return: square root of n
  */
-int _5_sqrt_recursion(void)
+
+
+int _sqrt_recursion(int n)
 {
-    return (0);
+	/* Create a base case */
+	if (n == 0)
+		return (0);
+	if (n < 0)
+		return (-1);
+	return (_guess_root(n, 1));
 }
