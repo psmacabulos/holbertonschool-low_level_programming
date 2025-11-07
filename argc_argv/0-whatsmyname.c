a7c93eb285b2fc5bfa86561d57d909bf73e84f2f
psmacabulos <patrick.macabulos@gmail.com>
2025-11-07 20:55:44 +0800

    Solved problem 0

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,11 +1,34 @@
+#include <stdio.h>
 #include "main.h"
+#include <unistd.h>
 
 /**
- * _0_whatsmyname - 
- * @
- * Return: 0
+ * _putchar - writes a character to stdout
+ * @c: character to print
+ * Return: 1 on success, -1 on error
  */
-int _0_whatsmyname(void)
+
+int _putchar(char c)
 {
-    return (0);
+	return (write(1, &c, 1));
+}
+
+/**
+ * main - program that prints its name followed by a new line.
+ * @argc: argument count
+ * @argv: argument vector (array of strings)
+ * Return: 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	char *p = argv[0];
+
+	(void)argc;
+	while (*p)
+	{
+		_putchar(*p);
+		p++;
+	}
+	_putchar('\n');
+	return (0);
 }
--- argc_argv/main.h
+++ argc_argv/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(int c);
+int _putchar(char c);
 
 
 
