9317bfd7fc6ca582b3b45f33a7f3c44ff39ee66f
psmacabulos <patrick.macabulos@gmail.com>
2025-11-07 22:11:17 +0800

    Created code for task 4

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,11 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
 
 /**
- * _4_add - 
- * @
- * Return: 0
+ * is_digit_string - checks if a string contains only digits
+ * @s: input string
+ * Return: 1 if all digits, 0 otherwise
  */
-int _4_add(void)
+int is_digit_string(char *s)
 {
-    return (0);
+
+	int i = 0;
+
+	while (s[i])
+	{
+		if (s[i] < '0' || s[i] > '9')
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+/**
+ * main - adds positive numbers
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 on success, 1 on error
+ */
+int main(int argc, char *argv[])
+{
+	int i, sum;
+
+	sum = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		if (!is_digit_string(argv[i]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum += atoi(argv[i]);
+	}
+
+	printf("%d\n", sum);
+	return (0);
 }
