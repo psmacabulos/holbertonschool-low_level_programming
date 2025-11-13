afc4f58b6736979825fd7878937e04eef039666e
psmacabulos <patrick.macabulos@gmail.com>
2025-11-13 17:33:11 +0800

    updated include and replaced main.h with dog.h

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "dog.h"
 
 /**
  * init_dog - initializes a variable of type struct dog
@@ -9,9 +9,6 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d == NULL)
-		return;
-
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
