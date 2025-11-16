52fdcc92767cea5b8bc3d3d6bbf5712471c9cec8
psmacabulos <patrick.macabulos@gmail.com>
2025-11-16 15:18:56 +0800

    Solved task 2

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -1,11 +1,26 @@
-#include "main.h"
+#include "function_pointers.h"
 
 /**
- * _2_int_index - 
- * @
- * Return: 0
+ * int_index - searches for an integer
+ * @array: array to be searched
+ * @size: size of the array
+ * @cmp: pointer to the function to be used to compare values
+ * Return: -1 if no match  and returns the int if theres a match
  */
-int _2_int_index(void)
+int int_index(int *array, int size, int (*cmp)(int))
 {
-    return (0);
+	if (size <= 0)
+		return (-1);
+
+	if (array && cmp)
+	{
+		int i;
+
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) != 0)
+				return (i);
+		}
+	}
+	return (-1);
 }
