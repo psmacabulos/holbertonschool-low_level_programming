c8e0c1dd586dd35592c67f61d38d64c35c178d1d
psmacabulos <patrick.macabulos@gmail.com>
2025-11-16 14:36:33 +0800

    Solved task 1

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,11 +1,16 @@
-#include "main.h"
+#include "function_pointers.h"
 
 /**
- * _1_array_iterator - 
- * @
- * Return: 0
+ * array_iterator - executes a function on each element of array
+ * @array: pointer to an array
+ * @size: size of the array
+ * @action: function pointer that will act on each element
  */
-int _1_array_iterator(void)
+void array_iterator(int *array, size_t size, void (*action)(int))
 {
-    return (0);
+	if (array && action)
+	{
+		for (size_t i = 0; i < size; i++)
+			action(array[i]);
+	}
 }
