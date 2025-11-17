1e350cefdb0e1aca903ae1f0c245bb22af14f83f
psmacabulos <patrick.macabulos@gmail.com>
2025-11-17 15:36:15 +0800

    Fixed problem on looping

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -1,37 +1,26 @@
 #include "variadic_functions.h"
-#include<stdlib.h>
-#include<stdarg.h>
-#include<stdio.h>
+#include <stdarg.h>
+#include <stdio.h>
+
 /**
- * print_numbers -prints numbers followed by a new line
+ * print_numbers - prints numbers followed by a new line
  * @separator: string to be printed between numbers
  * @n: number of integers passed to the function
- *
  */
-
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	unsigned int i = 0;
-	/* Declares the argument */
-	va_list args;	/* Initializes the list */
+	unsigned int i;
+	va_list args;
+
 	va_start(args, n);
 
-	if (separator == NULL)
-		return;
-	while (i < n)
+	for (i = 0; i < n; i++)
 	{
-		if (i == 0 || i == n)
-		{
-			printf("%d", va_arg(args, int));
-			i++;
-		}
-		else
-		{
-			printf("%s%d", separator, va_arg(args, int));
-			i++;
-		}
-
-
+		if (i > 0 && separator != NULL)
+			printf("%s", separator);
+		printf("%d", va_arg(args, int));
 	}
+
+	va_end(args);
 	printf("\n");
 }
