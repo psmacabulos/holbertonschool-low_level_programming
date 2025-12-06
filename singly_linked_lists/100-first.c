a03847b35b5311b8cb6238319a0de869f1cb32a3
psmacabulos <patrick.macabulos@gmail.com>
2025-12-06 13:36:11 +0800

    Tried a solution for the advanced task on Singly linked list

---
+++ singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * first - prints a message before main is executed
+ *
+ * Return: nothing
+ */
+void first(void) __attribute__((constructor));
+
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
