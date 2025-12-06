fcd781ad199a624cc1501b98efa2e7a99d3919ac
psmacabulos <patrick.macabulos@gmail.com>
2025-12-06 16:49:37 +0800

    Solved how to add a new node at the start of a doubly linked list

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -1,11 +1,28 @@
 #include "lists.h"
 
 /**
- * _2_add_dnodeint -
- * @
- * Return: 0
+ * add_dnodeint - adds a new node at the beginning of a dlistint_t list
+ * @head: double pointer to the head of the list
+ * @n: integer to store in the new node
+ *
+ * Return: address of the new element, or NULL if it failed
  */
-int _2_add_dnodeint(void)
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-    return (0);
+	dlistint_t *new;
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->prev = NULL;
+	new->next = *head;
+
+	if (*head != NULL)
+		(*head)->prev = new;
+
+	*head = new;
+
+	return (new);
 }
--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,11 +1,28 @@
 #include "lists.h"
 
 /**
- * _3_add_dnodeint_end -
- * @
- * Return: 0
+ * add_dnodeint - adds a new node at the beginning of a dlistint_t list
+ * @head: double pointer to the head of the list
+ * @n: integer to store in the new node
+ *
+ * Return: address of the new element, or NULL if it failed
  */
-int _3_add_dnodeint_end(void)
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-    return (0);
+	dlistint_t *new;
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->prev = NULL;
+	new->next = *head;
+
+	if (*head != NULL)
+		(*head)->prev = new;
+
+	*head = new;
+
+	return (new);
 }
