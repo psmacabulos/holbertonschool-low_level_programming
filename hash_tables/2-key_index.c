aba7b37caa56786bf894a89c86c546f291404a8f
psmacabulos <patrick.macabulos@gmail.com>
2025-12-08 18:36:44 +0800

    Get the index of the key

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -1,6 +1,16 @@
-/**
- * 2-key_index - placeholder
- *
- * Return: To be implemented
- */
 #include "hash_tables.h"
+
+/**
+ * key_index - gives the index of a key
+ * @key: the key string
+ * @size: size of the array of the hash table
+ *
+ * Return: index at which the key/value pair should be stored
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int hash;
+
+	hash = hash_djb2(key);
+	return (hash % size);
+}
