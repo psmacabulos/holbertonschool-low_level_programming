d6230a1df0fbe2c760237b5a479e84a51065e5fe
psmacabulos <patrick.macabulos@gmail.com>
2025-12-08 19:34:35 +0800

    Solve task to get the value of an index from a hash table

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -1,6 +1,29 @@
-/**
- * 4-hash_table_get - placeholder
- *
- * Return: To be implemented
- */
 #include "hash_tables.h"
+
+/**
+ * hash_table_get - retrieves a value associated with a key
+ * @ht: pointer to the hash table
+ * @key: the key string
+ *
+ * Return: value associated with the key, or NULL if not found
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *current;
+	unsigned long int index;
+
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	current = ht->array[index];
+
+	while (current != NULL)
+	{
+		if (strcmp(current->key, key) == 0)
+			return (current->value);
+		current = current->next;
+	}
+
+	return (NULL);
+}
