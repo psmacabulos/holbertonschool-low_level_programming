c00a8622617fd7402cd847d73b0e8ea57d0d1263
psmacabulos <patrick.macabulos@gmail.com>
2025-12-08 20:44:47 +0800

    Solve the task to free the hash table

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -1,6 +1,32 @@
-/**
- * 6-hash_table_delete - placeholder
- *
- * Return: To be implemented
- */
 #include "hash_tables.h"
+
+/**
+ * hash_table_delete - deletes a hash table
+ * @ht: pointer to the hash table
+ *
+ * Return: nothing
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t *current, *temp;
+	unsigned long int i;
+
+	if (ht == NULL)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		current = ht->array[i];
+		while (current != NULL)
+		{
+			temp = current->next;
+			free(current->key);
+			free(current->value);
+			free(current);
+			current = temp;
+		}
+	}
+
+	free(ht->array);
+	free(ht);
+}
