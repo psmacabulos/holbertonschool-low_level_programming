91fe398417e7ce608e9b12a7fa2dc3cb229a2e64
psmacabulos <patrick.macabulos@gmail.com>
2025-12-08 18:07:17 +0800

    Solve creating a hash function using djb2 algorithm

--- hash_tables/1-djb2.c
+++ hash_tables/1-djb2.c
@@ -1,6 +1,24 @@
-/**
- * 1-djb2 - placeholder
- *
- * Return: To be implemented
- */
 #include "hash_tables.h"
+
+/**
+ * hash_djb2 - implementation of the djb2 algorithm
+ * @str: string used to generate hash value
+ *
+ * Return: the calculated hash
+ */
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hash = 5381;
+	int c;
+	int i = 0;
+
+	/* Loop through each character until we reach the null terminator '\0' */
+	while (str[i] != '\0')
+	{
+		c = str[i];  /* get the ASCII value of the character */
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+		i++;  /* move to the next character */
+	}
+
+	return (hash);
+}
