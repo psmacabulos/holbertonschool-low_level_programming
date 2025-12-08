3d9d95a9a1ba4558f85a4bc6012d691a60f12480
psmacabulos <patrick.macabulos@gmail.com>
2025-12-08 20:21:40 +0800

    Solve task to print hash table contents

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -1,6 +1,32 @@
-/**
- * 5-hash_table_print - placeholder
- *
- * Return: To be implemented
- */
 #include "hash_tables.h"
+
+/**
+ * hash_table_print - prints a hash table
+ * @ht: pointer to the hash table
+ *
+ * Return: nothing
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *current;
+	unsigned long int i;
+	int first = 1;
+
+	if (ht == NULL)
+		return;
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		current = ht->array[i];
+		while (current != NULL)
+		{
+			if (first == 0)
+				printf(", ");
+			printf("'%s': '%s'", current->key, current->value);
+			first = 0;
+			current = current->next;
+		}
+	}
+	printf("}\n");
+}
